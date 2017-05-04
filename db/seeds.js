f7217eb1cea3c8d33a8d8fbf3a0f74847012e3f6
Eva Lai <evalai.dc@gmail.com>
2017-05-03 23:10:30 -0400

    created layouts of seed data and populated seeds.js file

---
+++ db/seeds.js
@@ -0,0 +1,18 @@
+const Driver = require('./models.js').Driver
+const Passenger = require('./models.js')
+
+
+const DriverSeedData = require('./DriverSeedData.json');
+const PassengerSeedData = require('./PassengerSeedData.json');
+
+Driver.remove({}, () =>{
+  Driver.create(DriverSeedData, () => {
+    process.exit();
+  });
+});
+
+Passenger.remove({}, () =>{
+  Passenger.create(PassengerSeedData, () => {
+    process.exit();
+  });
+});
